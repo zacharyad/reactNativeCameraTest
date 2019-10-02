21b0beb494f64b8381f49a6f302bcc10ff152080
zacharyAD <zacharyadcoding@gmail.com>
2019-10-02 15:26:23 -0400

    added the camera to appear as the initial view. Set up buttons that do not truly work. Need to send the data somewhere. Also played around with Location

---
+++ src/styles.js
@@ -0,0 +1,54 @@
+// src/styles.js
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width: winWidth, height: winHeight } = Dimensions.get('window');
+
+export default StyleSheet.create({
+  preview: {
+    height: winHeight,
+    width: winWidth,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+  },
+  key: {
+    height: winHeight,
+    width: winWidth,
+    position: 'relative',
+    left: '45%',
+    bottom: '-35%',
+    fontSize: 80,
+  },
+  alignCenter: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomToolbar: {
+    width: winWidth,
+    position: 'absolute',
+    height: 100,
+    bottom: 0,
+  },
+  captureBtn: {
+    width: 60,
+    height: 60,
+    borderWidth: 2,
+    borderRadius: 60,
+    borderColor: '#FFFFFF',
+  },
+  captureBtnActive: {
+    width: 80,
+    height: 80,
+  },
+  captureBtnInternal: {
+    width: 76,
+    height: 76,
+    borderWidth: 2,
+    borderRadius: 76,
+    backgroundColor: 'red',
+    borderColor: 'transparent',
+  },
+});
